7d890aa41077f0de77e5c6b2b6b7878b58f3100d
Diogo Henrique Ferreira <diogo.ferreira@unitri.edu.br>
2024-08-20 11:53:18 -0300

    fix: correção da conexão do mysql

--- src/app/DataBase/conexao.js
+++ src/app/DataBase/conexao.js
@@ -1,16 +1,32 @@
-import mysql from 'mysql2';
-import 'dotenv/config';
+import mysql from "mysql2";
+import "dotenv/config";
 
-const conexao = mysql.createConnection({
-    host: process.env.HOST,
-    port: process.env.PORT_DB,
-    user: process.env.USER,
-    password: process.env.PASSWORD_DB,
-    database: process.env.DATABASE,
-    connectionLimit: 10,
-    queueLimit: 0
-});
- 
-conexao.connect();
+const config = {
+  host: process.env.HOST,
+  port: process.env.PORT_DB,
+  user: process.env.USER,
+  password: process.env.PASSWORD_DB,
+  database: process.env.DATABASE
+};
 
-export default conexao;
+function handleDisconnect() {
+  const connection = mysql.createConnection(config);
+
+  connection.connect((err) => {
+    if (err) {
+      console.log("Erro ao conectar ao MySQL:", err);
+      setTimeout(handleDisconnect, 2000); // Tenta reconectar após 2 segundos
+    }
+  });
+
+  connection.on("error", (err) => {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.log("Conexão perdida, reconectando...");
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
