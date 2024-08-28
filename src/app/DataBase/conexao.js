9ed4b7c1e16248d5fdb4642ef038f231871e118e
Diogo Henrique Ferreira <diogo.ferreira@unitri.edu.br>
2024-08-28 10:56:22 -0300

    fix: tratamento de erros e keep-alive implementado para corrigir bug de conexão

--- src/app/DataBase/conexao.js
+++ src/app/DataBase/conexao.js
@@ -7,6 +7,10 @@ const config = {
   user: process.env.USER,
   password: process.env.PASSWORD_DB,
   database: process.env.DATABASE,
+  connectTimeout: 10000, // 10 segundos
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 };
 
 function handleDisconnect() {
@@ -14,8 +18,15 @@ function handleDisconnect() {
 
   connection.connect((err) => {
     if (err) {
-      console.log("Erro ao conectar ao MySQL:", err);
-      setTimeout(handleDisconnect, 2000); // Tenta reconectar após 2 segundos
+      if (err.code === 'ECONNREFUSED') {
+        console.error("Conexão recusada pelo servidor. Verifique se o MySQL está ativo e a configuração está correta.");
+        setTimeout(handleDisconnect, 5000); // Tenta reconectar após 5 segundos
+      } else {
+        console.error("Erro ao conectar ao MySQL:", err);
+        setTimeout(handleDisconnect, 2000); // Tenta reconectar após 2 segundos
+      }
+    } else {
+      console.log("Conectado ao MySQL!");
     }
   });
 
@@ -23,14 +34,25 @@ function handleDisconnect() {
     if (err.code === "PROTOCOL_CONNECTION_LOST") {
       console.log("Conexão perdida, reconectando...");
       handleDisconnect();
+    } else if (err.code === 'ECONNREFUSED') {
+      console.error("Conexão recusada pelo servidor. Tentando reconectar...");
+      setTimeout(handleDisconnect, 5000); // Tenta reconectar após 5 segundos
     } else {
-      throw err;
+      console.error("A conexão falhou:", err);
+      throw err; // Relança o erro se não for uma desconexão esperada
     }
   });
+
+  // Mantém a conexão ativa enviando consultas "keep-alive" a cada 1 hora
+  setInterval(() => {
+    connection.query('SELECT 1', (err) => {
+      if (err) console.error("Erro ao enviar keep-alive query:", err);
+    });
+  }, 60 * 60 * 1000); // 1 hora
 
   return connection;
 }
 
-const conexao = handleDisconnect()
+const conexao = handleDisconnect();
 
-export default conexao
+export default conexao;
