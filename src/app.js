4f7cf77c676b7a4e5c3736cd45496844d864aec4
Diogo Henrique Ferreira <diogo.ferreira@unitri.edu.br>
2024-09-13 13:34:55 -0300

    feat: número de requisições aumentado para o evento

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import rateLimit from 'express-rate-limit';
 const app = express();
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minuto
-  max: 30,
+  max: 300,
   validate: {
     xForwardedForHeader: false, // Desabilita a verificação do cabeçalho X-Forwarded-For
   },
